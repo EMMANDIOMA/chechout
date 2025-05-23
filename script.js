76d52ff813a4c59af7e7079b64fa9f1ad3f6c387
chidera <ofojuochidera2004@gmail.com>
2025-05-23 11:31:19 +0100

    second commit

--- script.js
+++ script.js
@@ -14,6 +14,63 @@ const Questions = [
     options: ["object", "Array", "string", "Number"],
     correct: 1,
   },
+  {
+    question: "what Data type make use of square curly braces ?",
+    options: ["object", "Array", "string", "Number"],
+    correct: 0,
+  },
+  {
+    question: " What does typeof 123 return ?",
+    options: ["object", "Array", "string", "Number"],
+    correct: 3,
+  },
+  {
+    question: " Which of the following is NOT a JavaScript data type?",
+    options: ["Boolean", "Float", "string", "Number"],
+    correct: 1,
+  },
+  {
+    question:
+      "What is the correct way to declare a variable in JavaScript (ES6+)?",
+    options: [
+      "var myVar = 10;",
+      "let myVar = 10;",
+      "const myVar = 10",
+      "All of the above",
+    ],
+    correct: 3,
+  },
+  {
+    question: "Which one checks both value and type in JavaScript?",
+    options: ["=", "==", "===", "!="],
+    correct: 2,
+  },
+  {
+    question:
+      "Who has been endorsed by the APC as its sole candidate for the 2027 presidential election?",
+    options: [
+      "Nasir El-Rufai",
+      "Yemi Osinbajo",
+      "Bola Tinub",
+      " Babajide Sanwo-Olu",
+    ],
+    correct: 2,
+  },
+  {
+    question: "How many maternal deaths were recorded in Nigeria in 2023?",
+    options: ["50,000", "25,000", "70,000", " 75,000"],
+    correct: 3,
+  },
+  {
+    question: " Which of the following plays was NOT written by Wole Soyinka?",
+    options: [
+      "The Lion and the Jewel",
+      "The Gods Are Not to Blame",
+      "A Dance of the Forests",
+      "Death and the King's Horseman",
+    ],
+    correct: 3,
+  },
 ];
 const DisplayQuestion = document.querySelector("#displayquestion");
 const score = document.querySelector("#score");
